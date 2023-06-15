ca182134beb7604e79952dd207460d312ae8aacf
js6495210 <js6495210@gmail.com>
2023-06-15 11:57:49 -0400

    Primer Commit

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function TodoItem({ todo, cambiarEstado }) {
+  const { id, task, completed } = todo;
+  const fnCambiarEstado = () =>{
+    cambiarEstado(id)
+  }
+  return (
+    <li className="list-group-item">
+      <input type="checkbox" id="check" className="form-checked-input me-2" onChange={fnCambiarEstado} checked={completed}/>
+      {task}
+
+    </li>  
+
+  )
+}
